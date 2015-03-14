96c7517f371b86fcc810c8156e1f8740054d9bc3
Richard Baptist <rpbaptist@gmail.com>
2015-03-14 14:07:18 +0100

    Changed hideous background colors

--- plugins/history-substring-search/history-substring-search.zsh
+++ plugins/history-substring-search/history-substring-search.zsh
@@ -132,8 +132,8 @@
 # configuration variables
 #-----------------------------------------------------------------------------
 
-HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=magenta,fg=white,bold'
-HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white,bold'
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=yellow,bold'
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='fg=red,bold'
 HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i'
 
 #-----------------------------------------------------------------------------
