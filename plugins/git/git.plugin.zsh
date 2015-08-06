9be67b8c36f950df3abb907c5cdfb6991286bf2b
Richard Baptist <richard@brightin.nl>
2015-08-06 15:23:24 +0200

    Easy way to set upstream.

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -136,6 +136,9 @@ alias gsd='git svn dcommit'
 # Will return the current branch name
 # Usage example: git pull origin $(current_branch)
 #
+
+function gbsu() { git branch -u origin/$1 }
+
 function current_branch() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || \
   ref=$(git rev-parse --short HEAD 2> /dev/null) || return
