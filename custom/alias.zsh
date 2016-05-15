8cd700e6a7e089b40891ec033d3361bc6a2359b7
Richard Baptist <rpbaptist@gmail.com>
2016-05-15 23:27:49 +0200

    Add custom aliases and functions

---
+++ custom/alias.zsh
@@ -0,0 +1,19 @@
+alias resh=". ~/.zshrc"
+alias zshenv="subl ~/.zshenv"
+alias ohmyzsh="subl ~/.oh-my-zsh"
+
+alias sudo='sudo '
+alias sl='subl'
+alias scp='noglob scp'
+alias ghostscript='/usr/bin/gs'
+alias toclip='xclip -sel clip <'
+
+alias dirsize='du -sch ./* | sort -h'
+
+alias git='hub'
+
+# Generate tags for sublime in a project.
+alias tags='ctags -R -f .tags'
+
+# Save files, save lives
+alias tp="trash-put"
---
+++ custom/function.zsh
@@ -0,0 +1,25 @@
+function hs() { heroku "$@" "--remote" "staging" }
+function hp() { heroku "$@" "--remote" "production" }
+
+function mysql-restore() {
+    echo $1;
+    (
+        echo "SET AUTOCOMMIT=0;"
+        echo "SET UNIQUE_CHECKS=0;"
+        echo "SET FOREIGN_KEY_CHECKS=0;"
+        cat "$1.sql"
+        echo "SET FOREIGN_KEY_CHECKS=1;"
+        echo "SET UNIQUE_CHECKS=1;"
+        echo "SET AUTOCOMMIT=1;"
+        echo "COMMIT;"
+    ) | mysql -u root -p "$1"
+}
+
+function ssl-req() {
+  local filename=`echo $1 | tr . _`
+  openssl req -nodes \
+    -newkey rsa:2048 \
+    -keyout $filename.key \
+    -out $filename.csr \
+    -subj "/C=NL/ST=Utrecht/L=Utrecht/O=Brightin/OU=Operations/CN=$1"
+}
