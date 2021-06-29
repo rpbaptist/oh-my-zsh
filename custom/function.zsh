9760875f9afb6e3c7712edbdedaf20226956689d
Richard Baptist <code@richardbaptist.nl>
2021-06-29 14:02:02 +0200

    Use fd in favor of find

--- custom/function.zsh
+++ custom/function.zsh
@@ -95,29 +95,29 @@ function notes() {
   fi
 }
 
-function shorthand-find-function() {
-  local path
-  local name
-  local type
-
-  type="$1"
-
-  if [ -z "$3" ]
-  then
-    path="."
-    name="$2"
-  else
-    path="$2"
-    name="$3"
-  fi
-
-  /usr/bin/find "$path" -type "$type" -name "$name"
-}
-
-function ff() {
-  shorthand-find-function "f" "$1" "$2"
-}
-
-function fd() {
-  shorthand-find-function "d" "$1" "$2"
-}
+# function shorthand-find-function() {
+#   local path
+#   local name
+#   local type
+#
+#   type="$1"
+#
+#   if [ -z "$3" ]
+#   then
+#     path="."
+#     name="$2"
+#   else
+#     path="$2"
+#     name="$3"
+#   fi
+#
+#   /usr/bin/find "$path" -type "$type" -name "$name"
+# }
+#
+# function ff() {
+#   shorthand-find-function "f" "$1" "$2"
+# }
+#
+# function fd() {
+#   shorthand-find-function "d" "$1" "$2"
+# }
